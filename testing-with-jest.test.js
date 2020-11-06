c94113ad4c8109c2994164cd777f339cb69d34f8
Rachael Stanislaw <rachaelstanislaw@gmail.com>
2020-11-06 13:16:23 -0800

    done

--- testing-with-jest.test.js
+++ testing-with-jest.test.js
@@ -8,13 +8,13 @@
 
 // --------------------1a) Create a test for a function called addThemUp that takes two numbers as arguments and returns the sum.
 
-
+// test first
 
 
 
 // --------------------1b) See the test fail. THEN write the code to make the test pass.
 
-
+// code second
 
 
 
